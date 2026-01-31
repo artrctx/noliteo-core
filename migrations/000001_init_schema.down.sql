bf602fff1d1d82ed6c5bff029d975f972b0d7bdb
artrctx <artrctx@gmail.com>
2026-01-31 10:35:36 -0600

    working on init db shcema

--- migrations/000001_init_schema.down.sql
+++ migrations/000001_init_schema.down.sql
@@ -1,2 +1,8 @@
 -- drop auth updated at trigger function
-DROP FUNCTION IF EXISTS auto_updated_at;
+DROP TABLE IF EXISTS usr;
+
+DROP FUNCTION IF EXISTS public.hash_token_trigger;
+
+DROP FUNCTION IF EXISTS public.auto_updated_at;
+
+DROP EXTENSION IF EXISTS pgcrypto;
--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,13 +1,24 @@
 -- Will be using built in gen_random_uuid()
 -- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE OR REPLACE FUNCTION auto_updated_at()
+CREATE OR REPLACE FUNCTION public.auto_updated_at()
 RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = now();
     RETURN NEW;
 END;
 $$ language 'plpgsql'; 
+
+CREATE OR REPLACE FUNCTION public.hash_token_trigger()
+RETURN TRIGGER
+LANGAUAGE plpgsql
+AS $function$DECLARE
+    api_key text;
+BEGIN
+
+END;
+$function$;
 
 
 CREATE TABLE usr(
@@ -17,3 +28,12 @@ CREATE TABLE usr(
     username Text,
     created_at timestamptz NOT NULL DEFAULT now()
 );
+
+
+-- ```-- Hashing apikey on insertCREATE OR REPLACE FUNCTION public.hash_apikey_trigger() RETURNS trigger LANGUAGE plpgsqlAS $function$DECLARE api_key text;BEGIN IF new.type='SANDBOX' THEN api_key := 'sk_test_' || new.secret; ELSE api_key := 'sk_live_' || new.secret; END IF;
+--  IF EXISTS (SELECT * FROM validate_apikey_hash(('{"key": "' || api_key || '"}')::jsonb)) THEN RAISE EXCEPTION 'Duplicate active API key detected'; END IF;
+--  -- Add prefix as identifier new.key_prefix := substring(api_key, 1, 7) || '...' || substring(api_key, LENGTH(api_key) - 5);
+--  -- Generate a bcrypt hash new.secret := crypt(api_key, gen_salt('bf'));
+--  return new;END;
+-- ``````CREATE OR REPLACE FUNCTION public.validate_apikey_hash(args jsonb) RETURNS TABLE(id text, org_id text, type environment) LANGUAGE plpgsqlAS $function$DECLARE hash text;BEGIN -- https://www.postgresql.org/docs/9.5/functions-json.html RETURN QUERY SELECT a.id, a.org_id, a.type FROM "ApiKeys" a WHERE a.enabled = true AND (expiration > NOW() OR expiration IS NULL) AND (args#>>'{orgIds,0}' IS NULL OR (args->'orgIds') ? a.org_id) AND (a.secret = crypt(args->>'key', a.secret));END;
+-- $function$;```
